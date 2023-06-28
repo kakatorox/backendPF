0dc55ae150f730a55305336ff10fe2dbf74e5be3
KAKATO <alex.ortiz@valposystems.com>
2023-06-27 23:58:32 -0400

    15

--- index.js
+++ index.js
@@ -3,26 +3,24 @@ require('dotenv').config();
 const routes = require('./src/routes/routes')
 const cors = require('cors');
 const helmet = require('helmet');
-
-const app = express()
+const app = express();
 const port = process.env.PORT || 8080;
 
-const whitelist = ["http://localhost:3000", "http://127.0.0.1:3000","https://frontpf2.onrender.com"];
+const whitelist = ["http://localhost:3000", "http://127.0.0.1:3000", "https://frontpf2.onrender.com"];
 
 const corsOptions = {
-    origin: function (origin, callback){
-            if(whitelist.indexOf(origin) !== -1 || !origin){
-                callback(null, true);
-            } else {
-                callback(new error("no permitido por CORS"))
-            }
+    origin: function (origin, callback) {
+        if (whitelist.includes(origin) || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error("No permitido por CORS"));
         }
+    },
 };
 
 app.use(cors(corsOptions));
 app.use(helmet());
-
 app.use(express.json());
 app.use(routes);
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`La aplicación se está ejecutando en el puerto ${port}!`));

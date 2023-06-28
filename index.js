250408ec53f1118c54ed3dc8bfe33cbbb0c30c8d
KAKATO <alex.ortiz@valposystems.com>
2023-06-27 23:27:37 -0400

    11

--- index.js
+++ index.js
@@ -7,20 +7,21 @@ const helmet = require('helmet');
 const app = express()
 const port = process.env.PORT || 8080;
 
-const whitelist = ["http://localhost:3000", "http://127.0.0.1:3000","https://master--elegant-cassata-382c2f.netlify.app/"];
+// const whitelist = ["http://localhost:3000", "http://127.0.0.1:3000","https://master--elegant-cassata-382c2f.netlify.app/"];
 
-const corsOptions = {
-    origin: function (origin, callback){
-            if(whitelist.indexOf(origin) !== -1 || !origin){
-                callback(null, true);
-            } else {
-                callback(new error("no permitido por CORS"))
-            }
-        }
-};
+// const corsOptions = {
+//     origin: function (origin, callback){
+//             if(whitelist.indexOf(origin) !== -1 || !origin){
+//                 callback(null, true);
+//             } else {
+//                 callback(new error("no permitido por CORS"))
+//             }
+//         }
+// };
 
-app.use(cors(corsOptions));
-app.use(cors());
+// app.use(cors(corsOptions));
+// app.use(cors());
+app.options('*', cors(corsOptions));
 app.use(helmet());
 
 app.use(express.json());
